83202b515f8772adfd84b45419b91e4e0a375f03
Paul BRETON <paul.breton@cooperl.com>
2025-03-22 12:34:01 +0100

    feat(login): responsive login page

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,6 +1,11 @@
+
+<script setup lang="ts">
+const { isMobile } = useResponsive()
+</script>
+
 <template>
   <div class="h-screen flex overflow-hidden">
-    <div class="bg-[url(/assets/images/background-login-2.png)] bg-center h-full w-2/3" />
+    <div v-if="!isMobile" class="bg-[url(/assets/images/background-login-2.png)] bg-center h-full w-2/3" />
     <div class="m-auto grow p-6">
       <slot />
     </div>
